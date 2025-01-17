ae0b678fd9f29acb4214e05e8963b910be3c0041
petsamuel <petsamuel4@gmail.com>
2025-01-17 12:45:28 +0100

    initial commit

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,6 +1,6 @@
 import { Header } from "../components/index";
 
-const LadingPage = () => {
+const LandingPage = () => {
   return (
     <div className=" dark:bg-black bg-white  h-[150svh] bg-opacity-10 w-[svw] flex justify-center ">
       <div className="">
@@ -10,4 +10,4 @@ const LadingPage = () => {
   );
 };
 
-export default LadingPage;
+export default LandingPage;
